33a96017c0ea472f1c1cba4fc85051e91bad0528
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-05-15 22:08:35 +0200

    Switched to Angular

--- scripts/controllers/LoginController.js
+++ scripts/controllers/LoginController.js
@@ -1,4 +1,19 @@
 angular.module('Login')
     .controller('LoginController', function ($scope, $route, $location) {
-    	$scope.titletextbende = "test";
+       
+
+        $scope.loginUser = function (e) {
+            var username = $scope.username;
+            var password = $scope.password;
+
+            //TODO server bende.
+
+            $location.path("/Feed");
+            $location.replace();
+        };
+
+        $scope.redirectRegister = function (e) {
+            $location.path("/Register");
+            $location.replace();
+        };
     });
--- scripts/modules/doeIetsModule.js
+++ scripts/modules/doeIetsModule.js
@@ -1,4 +1,4 @@
-var app = angular.module('doeIets', ['ngRoute', 'Login']);
+var app = angular.module('doeIets', ['ngRoute', 'Login', 'Register', 'Feed', 'Profile', 'Options', 'Privacy']); //Dut niet als ik hier Options en Privacy bij gooi..
 
 
 app.config(['$routeProvider', function ($routeProvider) {
@@ -7,6 +7,31 @@ app.config(['$routeProvider', function ($routeProvider) {
             //Login
             templateUrl: 'views/Login/Login.html',
             controller: 'LoginController',
+        })
+        .when('/Register', {
+            //Register
+            templateUrl: 'views/Login/Register.html',
+            controller: 'RegisterController',
+        })
+        .when('/Feed', {
+            //Feed
+            templateUrl: 'views/Application/Feed.html',
+            controller: 'FeedController',
+        })
+        .when('/Profile', {
+            //Profile
+            templateUrl: 'views/Application/Profile.html',
+            controller: 'ProfileController',
+        })
+        .when('/Options', {
+            //Options
+            templateUrl: 'views/Application/Options.html',
+            controller: 'OptionsController',
+        })
+        .when('/Privacy', {
+            //Privacy
+            templateUrl: 'views/Application/Privacy.html',
+            controller: 'PrivacyController',
         })
         .otherwise({
             //Login
