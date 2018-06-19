cc50fc3e8c6a691ff1d43303c490f203314e72f5
doomedxx <wesley.dw@live.nl>
2018-06-20 01:13:03 +0200

    Alles weg?

--- project2.4/project2.4/scripts/modules/ControllerModules.js
+++
@@ -1,9 +0,0 @@
-angular.module('Login', ['ngRoute', 'httpRequests']);
-angular.module('Register', ['ngRoute', 'httpRequests']);
-angular.module('Feed', ['ngRoute', 'httpRequests']);
-angular.module('Profile', ['ngRoute', 'httpRequests']);
-angular.module('Options', ['ngRoute', 'httpRequests']);
-angular.module('Privacy', ['ngRoute', 'httpRequests']);
-angular.module('Friends', ['ngRoute', 'httpRequests']);
-angular.module('Logout', ['ngRoute', 'httpRequests']);
-angular.module('mapApp', ['ngRoute', 'httpRequests']);
--- project2.4api/project2.4/project2.4.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace project2._4.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
