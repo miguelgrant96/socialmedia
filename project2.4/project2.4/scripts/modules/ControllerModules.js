07508a3138d5e9d8e904f51d4d56ff1322cad379
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-06-20 01:13:56 +0200

    Master kleeg

--- project2.4/project2.4/scripts/modules/ControllerModules.js
+++
@@ -1,9 +0,0 @@
-angular.module('Login', ['ngRoute', 'httpRequests', 'Token']);
-angular.module('Register', ['ngRoute', 'httpRequests', 'Token']);
-angular.module('Feed', ['ngRoute', 'httpRequests']);
-angular.module('Profile', ['ngRoute', 'httpRequests']);
-angular.module('Options', ['ngRoute', 'httpRequests']);
-angular.module('Privacy', ['ngRoute', 'httpRequests']);
-angular.module('Friends', ['ngRoute', 'httpRequests']);
-angular.module('Logout', ['ngRoute', 'httpRequests']);
-angular.module('mapApp', ['ngRoute', 'httpRequests']);
--- project2.4api/project2.4/project2.4.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="project2._4.API.WebApiApplication" Language="C#" %>
