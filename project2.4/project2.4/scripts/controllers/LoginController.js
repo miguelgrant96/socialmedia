2a0ff8d1f4243bd1497687eee1b0abfd6b92d798
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-06-21 15:24:18 +0200

    Got miself a master

--- project2.4/project2.4/scripts/controllers/LoginController.js
+++ project2.4/project2.4/scripts/controllers/LoginController.js
@@ -12,7 +12,8 @@ angular.module('Login')
             //Sloop deze eruit dan werkt login weer
             //$location.path("/Feed");
             //$location.replace();
-
+            $location.path("/Feed");
+            $location.replace();
             //TODO server bende.
             AuthorizationService.Authorize(username, password).then((Response) => {
                 TokenService.SetAccessToken(Response.data.access_token);
