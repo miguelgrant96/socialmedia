94bfd117a3f2b26200d075beab7614cfea2979e5
doomedxx <wesley.dw@live.nl>
2018-06-24 02:30:45 +0200

    Fixed Profile Page

--- project2.4/project2.4/scripts/controllers/ProfileController.js
+++ project2.4/project2.4/scripts/controllers/ProfileController.js
@@ -1,5 +1,52 @@
 ﻿angular.module('Profile')
-    .controller('ProfileController', function ($scope, $route, $location) {
+    .controller('ProfileController', function ($scope, $route, $location, $timeout, UriBuilder, httpRequestService) {
+
+
+        //Getting ProfileInfo and Feed 
+        var url = UriBuilder.BuildUrl("ProfileInfo", { 'id': null });
+        httpRequestService.getRequest(url, function success(response) {
+            $scope.ProfileInfo = response.data;
+            var url = UriBuilder.BuildUrl("Feed", { 'id': $scope.ProfileInfo.Id });
+            httpRequestService.getRequest(url, function success(response) {
+                $scope.Feed = response.data;
+            }, function fail(response) {
+                console.log("Ging iets fout bij het ophalen van de Feed");
+            });
+        }, function fail(response) {
+            console.log("Ging iets fout bij het ophalen van het Profile");
+        });
+
+
+        var url = UriBuilder.BuildUrl("Account", { 'id': null });
+        httpRequestService.getRequest(url, function success(response) {
+            $scope.Account = response.data;
+        }, function fail(response) {
+            console.log("Ging iets fout bij het ophalen van het account");
+        });
+
+        $timeout(function () {
+            var url = UriBuilder.BuildUrl("Feed");
+            httpRequestService.getRequest(url, function success(response) {
+                $scope.Feed = response.data;
+            }, function fail(response) {
+                console.log("Ging iets fout bij het ophalen van de Feed");
+            });
+        }, 1000);
+
+        $scope.PostFeed = function () {
+            var feedtext = $scope.NewPostText;
+            var url = UriBuilder.BuildUrl("Feed", { 'Text': feedtext, 'imageurl': "", 'videourl': "" });
+            httpRequestService.PostRequest(url, null, function success(response) {
+                var url = UriBuilder.BuildUrl("Feed");
+                httpRequestService.getRequest(url, function success(response) {
+                    $scope.Feed = response.data;
+                }, function fail(response) {
+                    console.log("Ging iets fout bij het ophalen van de Feed");
+                });
+            }, function fail(response) {
+                    console.log("niet helemaal");
+                });
+        }
 
         $scope.redirectFeed = function (e) {
             $location.path("/Feed");
