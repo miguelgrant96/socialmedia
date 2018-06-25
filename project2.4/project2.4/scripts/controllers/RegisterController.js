c9bdbb0e97d7de8d72fa5373dec5ce3d497604a9
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-06-25 19:23:56 +0200

    Update RegisterController.js

--- project2.4/project2.4/scripts/controllers/RegisterController.js
+++ project2.4/project2.4/scripts/controllers/RegisterController.js
@@ -1,5 +1,5 @@
-﻿angular.module('Register')
-    .controller('RegisterController', function ($scope, $route, $location, UriBuilder, httpRequestService, TokenService) {
+angular.module('Register')
+    .controller('RegisterController', function ($scope, $route, $location, UriBuilder, httpRequestService, TokenService, AuthorizationService) {
 
         
 
@@ -12,7 +12,7 @@
             var password = $scope.password;
             var url = UriBuilder.BuildUrl("Account");
             var data = { 'FirstName': firstName, 'LastName': lastName, 'Password': password, 'Gender': gender, 'Email': email, 'BirthDate': birthdate };
-            httpRequestService.PostRequest(url, data, function success(response) { //Deze werkt niet
+            httpRequestService.PostRequest(url, data, function success(response) { 
                 AuthorizationService.Authorize(email, password).then((Response) => {
                     TokenService.SetAccessToken(Response.data.access_token);
                     $location.path("/Feed"); //Redirecten naar Options? om direct instellingen aan te passen?
@@ -20,8 +20,11 @@
                 }).catch((Response) => {
                     console.log("niet ingelogt");
                 });
+
             }, function fail(response) {
-                console.log("registratie niet helemaal");
+                console.log("registratie niet gelukt: ");
+                if (response.data != null)
+                    console.log(response.data.ExceptionMessage);
             });
         }
 
