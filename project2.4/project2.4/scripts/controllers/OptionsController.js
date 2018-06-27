f70e8cef5549b566b5b58735b07c119b27e789ec
Marnick Scholten <m.f.scholten@st.hanze.nl>
2018-06-27 02:48:28 +0200

    Responsive,

--- project2.4/project2.4/scripts/controllers/OptionsController.js
+++ project2.4/project2.4/scripts/controllers/OptionsController.js
@@ -27,6 +27,21 @@ angular.module('Options')
             }
         }
 
+        // upload on file select or drop
+        //$scope.upload = function (file) {
+        //    Upload.upload({
+        //        url: 'Images/profilePics/',
+        //        data: { file: file }
+        //    }).then(function (resp) {
+        //        console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        //    }, function (resp) {
+        //        console.log('Error status: ' + resp.status);
+        //    }, function (evt) {
+        //        var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+        //        console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        //    });
+        //};
+
 
         $scope.UpdateProfile = function () {
             if ($scope.ProfileInfo.NewPic == null) {
